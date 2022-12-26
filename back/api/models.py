96a38acfb9723f5b8742f3220f7fc5d5ecb714af
fawzibazari <fawzi.bazari2@gmail.com>
2022-12-26 12:22:28 +0100

    [sp1-be-02] first migration for user callection

---
+++ back/api/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Users(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='')
+    mail = models.CharField(max_length=200,blank=False, default='')
+    password = models.BooleanField(default=False)
