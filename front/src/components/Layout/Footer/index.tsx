56cdba7359a5f1635d7cac0b066c84252d7eff70
raphael-co <76869389+raphael-co@users.noreply.github.com>
2022-12-26 13:53:45 +0100

    [sp1-fe-11A] footer

--- front/src/components/Layout/Footer/index.tsx
+++ front/src/components/Layout/Footer/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-// import { Navbar } from "react-bootstrap";
-// import { ReactComponent as HeartIcon } from "../../../assets/img/heart.svg";
+import styles from "./styles.module.scss";
 
 const Footer = () => {
-  return <></>;
+  function goRepo() {
+    window.location.href = "https://github.com/WILKmichal/euh/tree/main";
+  }
+
+  return (
+    <div className="container-fluid text-center text-md-left">
+      <hr />
+      <div className="row">
+        <div>
+          <h5 className="text-uppercase">Michal Fawzi Raphael</h5>
+          <span onClick={goRepo} className={styles.ForgotPassword}>
+            Github
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
